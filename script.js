8c033897e23348232d92abf722b68f220f871f7e
zudane <zudane@inbox.lv>
2022-04-17 19:11:32 +0300

    Lailas_chats

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(1)
+let zina = document.querySelector('.manaZina');
+let zinas = document.querySelector('.chataZinas');
+
+function sutitZinu()
+{
+    console.log('sutitZinu() darbojas');
+
+    zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+}
+
+async function ieladetChataZinas()
+{
+    let datiNoServera = await fetch('chatazinas.txt');
+    let dati = await datiNoServera.text();
+    zinas.innerHTML = dati;
+}
+
+setInterval( ieladetChataZinas, 1000 )
