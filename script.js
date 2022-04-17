586b90580bde543512c7fb2e4edff892f6fc96c2
zudane <zudane@inbox.lv>
2022-04-17 23:05:16 +0300

    Lailas_Chats6

--- script.js
+++ script.js
@@ -18,5 +18,20 @@ async function ieladetChataZinas()
     let dati = await datiNoServera.text();
     zinas.innerHTML = dati;
 }
+async function ieladetChataZinasJson()
+{
+    let datiNoServera = await fetch(API + '/lasit');
+    let dati = await datiNoServera.json();
+    
+    //console.log(await dati[0]['zina'])
+    zinas.innerHTML= '';
+    i=0;
+    while (i < await dati.length)
+    {
+        console.log(i);
+        zinas.innerHTML =zinas.innerHTML+dati[i]['vards']+': '+dati[i['zina']+ '<br />';
+        i=i+1;
+    }
+}//beidzas ieladetChataZinasJson
 
-setInterval( ieladetChataZinas, 1000 )
+setInterval( ieladetChataZinasJson, 1000 )
