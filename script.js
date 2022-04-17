5cd0b81348d7e1b1b685084855d8ed5e0b72e5c1
zudane <zudane@inbox.lv>
2022-04-17 22:30:47 +0300

    Lailas_chats5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const API="https://Chat2022L.lailazudne.repl.co"
 let zina = document.querySelector('.manaZina');
+let vards = document.querySelector('.vards');
 let zinas = document.querySelector('.chataZinas');
 
 function sutitZinu()
@@ -8,7 +9,7 @@ function sutitZinu()
 
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
 
-    fetch(API + '/sutit/Laila/' + zina.value)
+    fetch(API + '/sutit/'+ vards.value +'/' + zina.value)
 }
 
 async function ieladetChataZinas()
